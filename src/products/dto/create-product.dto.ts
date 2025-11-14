847f8f3b933c24de34d392a9104eb744b7fc8746
rtnkwami <nukukwami@outlook.com>
2025-11-14 09:54:54 +0000

    fix(products): fix missing validations for create and update product

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,8 +1,38 @@
+import {
+  IsArray,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  IsUrl,
+} from 'class-validator';
+
 export class CreateProductDto {
+  @IsNotEmpty()
+  @IsString()
   name: string;
+
+  @IsOptional()
+  @IsString()
   description: string;
+
+  @IsNotEmpty()
+  @IsString()
   category: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @IsPositive()
   price: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @IsPositive()
   stock: number;
+
+  @IsOptional()
+  @IsArray()
+  @IsUrl({}, { each: true })
   images?: string[];
 }
