07bfa49789132a5c1b852a766fbb14fa70a82ab6
rtnkwami <nukukwami@outlook.com>
2025-11-27 06:03:26 +0000

    feat(api): add api health check

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,8 +1,26 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
-  @Get('health')
+  @ApiOperation({
+    summary: 'API Health Check',
+    description: 'Verifies that the API is healthy',
+  })
+  @ApiResponse({
+    status: 200,
+    schema: {
+      type: 'object',
+      properties: {
+        status: { type: 'string', example: 'ok' },
+        timestamp: {
+          type: 'string',
+          format: 'date-time',
+        },
+      },
+    },
+  })
+  @Get()
   health() {
     return {
       status: 'ok',
