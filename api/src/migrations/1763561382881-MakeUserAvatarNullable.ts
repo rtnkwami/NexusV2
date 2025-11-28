41c84d2177c73b70a95ba4d92f6127078fe7af1f
rtnkwami <nukukwami@outlook.com>
2025-11-28 13:35:40 +0000

    feat(api): lint and format migrations

--- api/src/migrations/1763561382881-MakeUserAvatarNullable.ts
+++ api/src/migrations/1763561382881-MakeUserAvatarNullable.ts
@@ -1,14 +1,17 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class MakeUserAvatarNullable1763561382881 implements MigrationInterface {
-    name = 'MakeUserAvatarNullable1763561382881'
+  name = 'MakeUserAvatarNullable1763561382881';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "avatar" DROP NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "user" ALTER COLUMN "avatar" DROP NOT NULL`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "avatar" SET NOT NULL`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "user" ALTER COLUMN "avatar" SET NOT NULL`,
+    );
+  }
 }
