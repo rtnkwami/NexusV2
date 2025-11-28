f78f86e6eba4ee1aba09d607612d3d5fee401b1d
rtnkwami <nukukwami@outlook.com>
2025-11-28 13:12:17 +0000

    feat(tests/api): add overrides to product test util for more control

--- api/test/utils/fakeProducts.ts
+++ api/test/utils/fakeProducts.ts
@@ -1,6 +1,17 @@
 import { faker } from '@faker-js/faker';
 
-export default function createFakeProduct() {
+type FakeProduct = {
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  stock: number;
+  images: string[];
+};
+
+export default function createFakeProduct(
+  overrides?: Partial<FakeProduct>,
+): FakeProduct {
   const imageArray: string[] = [];
 
   for (let index = 0; index < 3; index++) {
@@ -8,15 +19,14 @@ export default function createFakeProduct() {
     imageArray.push(image);
   }
 
-  return {
+  const product = {
     name: faker.commerce.product(),
     description: faker.commerce.productDescription(),
-    category: faker.helpers.arrayElement([
-      'Electronics',
-      faker.commerce.department(),
-    ]),
+    category: faker.commerce.department(),
     price: parseFloat(faker.commerce.price()),
     stock: faker.number.int({ min: 0, max: 200 }),
     images: imageArray,
   };
+
+  return { ...product, ...(overrides ?? {}) };
 }
