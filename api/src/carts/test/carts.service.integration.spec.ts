80ff0082073bb6cde5f15acb4b2542c1fd8101ed
rtnkwami <nukukwami@outlook.com>
2025-12-02 10:25:37 +0000

    feat(tests/api): add test for getting a cart

--- api/src/carts/test/carts.service.integration.spec.ts
+++ api/src/carts/test/carts.service.integration.spec.ts
@@ -1,18 +1,20 @@
 import KeyvRedis from '@keyv/redis';
-import { CacheModule } from '@nestjs/cache-manager';
+import { CACHE_MANAGER, CacheModule } from '@nestjs/cache-manager';
 import { Test, TestingModule } from '@nestjs/testing';
-import { beforeAll, it, expect, describe, afterAll } from 'vitest';
+import { beforeAll, it, expect, describe, afterAll, beforeEach } from 'vitest';
 import { RedisContainer, StartedRedisContainer } from '@testcontainers/redis';
 import { CartsService } from '../carts.service';
 import createFakeProduct from 'test/utils/fakeProducts';
 import { randomUUID } from 'crypto';
 import { CartItem } from 'src/orders/orders.service';
 import { faker } from '@faker-js/faker';
+import { Cache } from 'cache-manager';
 
 describe('CartService (Integration)', () => {
   let testRedis: StartedRedisContainer;
   let service: CartsService;
   let module: TestingModule;
+  let cacheManager: Cache;
 
   beforeAll(async () => {
     testRedis = await new RedisContainer('redis:8.4.0').start();
@@ -31,6 +33,7 @@ describe('CartService (Integration)', () => {
     }).compile();
 
     service = module.get<CartsService>(CartsService);
+    cacheManager = module.get<Cache>(CACHE_MANAGER);
   });
 
   afterAll(async () => {
@@ -39,6 +42,10 @@ describe('CartService (Integration)', () => {
   });
 
   describe('basic CRUD operations', () => {
+    beforeEach(async () => {
+      await cacheManager.clear();
+    });
+
     it('should put items in a cart', async () => {
       const data: { cart: CartItem[] } = {
         cart: [],
@@ -59,6 +66,28 @@ describe('CartService (Integration)', () => {
       expect(cartData?.cart).toHaveLength(1);
       expect(cartData?.cart[0]).toHaveProperty('name');
       expect(cartData?.cart[0].name).toBe(baseProduct.name);
+    });
+
+    it('should get a cart', async () => {
+      const data: { cart: CartItem[] } = {
+        cart: [],
+      };
+      const baseProduct = createFakeProduct();
+
+      const cartItem: CartItem = {
+        id: randomUUID(),
+        name: baseProduct.name,
+        price: baseProduct.price,
+        quantity: faker.number.float({ fractionDigits: 2 }),
+        image: baseProduct.images[0],
+      };
+
+      const cartKey = 'test-key';
+      data.cart.push(cartItem);
+      await service.updateCart(cartKey, data);
+
+      const result = await service.getCart(cartKey);
+      expect(result?.cart).toHaveLength(1);
     });
   });
 });
