b97be1942034e51ef6e2942045cb6144e724ffde
rtnkwami <nukukwami@outlook.com>
2025-12-16 08:09:43 +0000

    build(web): add shadcn to web package

--- packages/web/nuxt.config.ts
+++ packages/web/nuxt.config.ts
@@ -1,5 +1,19 @@
+import tailwindcss from '@tailwindcss/vite'
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['~/assets/css/tailwind.css'],
+  shadcn: {
+    prefix: '',
+    componentDir: '@/components/ui'
+  },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
+  modules: ['shadcn-nuxt', '@nuxt/image'],
 })
