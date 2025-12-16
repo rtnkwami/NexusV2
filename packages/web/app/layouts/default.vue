113db89fdd1df93244208634382edca4d396cb1b
rtnkwami <nukukwami@outlook.com>
2025-12-16 08:53:23 +0000

    feat(web): add login component and default page layout

---
+++ packages/web/app/layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div
+    class="relative flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10
+           bg-[url('https://miro.medium.com/v2/1*FsJUFXiqszwqq9JrtlIDpQ.jpeg')] bg-cover bg-center"
+  >
+
+    <div class="absolute inset-0 bg-black/50"></div>
+
+    <div class="relative z-10 w-full">
+      <slot />
+    </div>
+  </div>
+</template>
