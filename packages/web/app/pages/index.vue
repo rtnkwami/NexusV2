3b0c7ae1d43cf3dc532dd0309065fe3036e2d597
rtnkwami <nukukwami@outlook.com>
2025-12-16 08:54:38 +0000

    feat(web): add basic home page and login page

---
+++ packages/web/app/pages/index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+  useHead({ title: 'Atelier' })
+</script>
+
+<template>
+  <h1>Hello World</h1>
+  <p>this is me!</p>
+</template>
---
+++ packages/web/app/pages/login/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+  useHead({ title: 'Atelier | Login' })
+  
+  import { GalleryVerticalEnd } from "lucide-vue-next"
+  import LoginForm from "@/components/login-03/components/LoginForm.vue";
+</script>
+
+<template>
+  <div class="flex w-full max-w-sm mx-auto flex-col gap-6">
+    <a href="#" class="flex items-center gap-2 self-center font-medium text-white">
+      <div class="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+        <GalleryVerticalEnd class="size-4" />
+      </div>
+      Atelier
+    </a>
+
+    <LoginForm />
+  </div>
+</template>
